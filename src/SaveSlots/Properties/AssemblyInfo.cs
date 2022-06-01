e4cfd3e086851ffccb32a7dd6d539c3875b257f1
bkbkbkbkbkbk <42798415+bkbkbkbkbkbk@users.noreply.github.com>
2022-06-01 23:51:46 +1000

    pumped version number for saveslot mod

--- src/SaveSlots/Properties/AssemblyInfo.cs
+++ src/SaveSlots/Properties/AssemblyInfo.cs
@@ -34,8 +34,8 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.1.0")]
-[assembly: AssemblyFileVersion("1.0.1.0")]
+[assembly: AssemblyVersion("1.1.0.0")]
+[assembly: AssemblyFileVersion("1.1.0.0")]
 
-[assembly: MelonInfo(typeof(SaveSlots.Main), "BPH SaveSlots", "1.0.1", "bk")]
+[assembly: MelonInfo(typeof(SaveSlots.Main), "BPH SaveSlots", "1.1.0", "bk")]
 [assembly: MelonGame("UNKNOWN", "Backpack Hero")]
